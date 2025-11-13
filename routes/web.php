6c0bdc5ce8681381e6c79f4d5cf66e33e5544e46
Yousra-akg <akagouyoussra@gmail.com>
2025-11-13 12:22:31 +0100

    commit du chapitre Forms - CSRF

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/ping', fn() => 'pong');
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::post('/articles',        [ArticleController::class, 'store'])->name('articles.store');
 
-Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('/a-propos', [PageController::class, 'about'])->name('about');
 
-// Mini-routes articles (mockées pour l’instant)
-Route::get('/articles', [PageController::class, 'articles'])->name('articles.index');
-Route::get('/articles/{slug}', [PageController::class, 'show'])->name('articles.show');
 
-Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
